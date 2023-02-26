eaa7ab76f6bcea14d5bb206ba7f837308c3ba1fe
AlexandrK <alex.krauze@outlook.com>
2023-02-26 20:17:09 +0200

    Add project files.

---
+++ MyCompany/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿
+namespace MyCompany.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string EmpName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Position { get; set; }
+        public string Phone { get; set; }
+
+        public int DepartmentsId { get; set; }
+        public virtual Department Department { get; set; }
+    }
+}
